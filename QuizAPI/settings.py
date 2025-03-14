13d8958d93530b217b9ec74d45c90e82ef4a05f4
rawlife247 <118503669+rawlife247@users.noreply.github.com>
2025-03-14 10:33:31 +0000

    Enable database configuration in settings.py

--- QuizAPI/settings.py
+++ QuizAPI/settings.py
@@ -79,10 +79,10 @@ WSGI_APPLICATION = 'QuizAPI.wsgi.application'
 # Database
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
