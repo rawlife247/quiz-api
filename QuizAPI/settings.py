0508f4fee36b72b98356f17f67082df95df6bacb
rawlife247 <118503669+rawlife247@users.noreply.github.com>
2025-03-14 10:32:04 +0000

    Uncomment database configuration in settings.py to enable dynamic database settings

--- QuizAPI/settings.py
+++ QuizAPI/settings.py
@@ -79,14 +79,14 @@ WSGI_APPLICATION = 'QuizAPI.wsgi.application'
 # Database
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 
